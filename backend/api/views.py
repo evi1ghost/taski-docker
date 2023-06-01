470529b7f973b44f779cd62a15cd4ece95ec7ebe
evi1ghost <evil_ghost@bk.ru>
2023-06-01 10:03:31 +0300

    add setup.cfg amd fix PEP8

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.response import Response
-from rest_framework import status
 
 from .models import Task
 from .serializers import TaskSerializer
@@ -14,4 +13,3 @@ class TaskView(viewsets.ModelViewSet):
         serializer = self.get_serializer(self.get_object())
         super().destroy(*args, **kwargs)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
